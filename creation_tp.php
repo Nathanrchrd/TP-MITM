17d96e3ce1850b371cb498aa6fcee7b9c1859a87
Eliott HUBERT <e.hubert@eleve.leschartreux.net>
2022-03-31 16:34:02 +0200

    ajout briefing

--- creation_tp.php
+++ creation_tp.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>creation-TP</title>
-        <link rel="stylesheet" href="styles.css" type="text/css" />
+        <link rel="stylesheet" href="Styles/styles.css" type="text/css" />
         <?php
            session_set_cookie_params(1);
            session_start();
