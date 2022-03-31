aefb5a755676690b770d8ecf2b21aad60cb86230
BOUVIER Gaston <g.bouvier@eleve.leschartreux.net>
2022-03-31 17:42:56 +0200

    avancé

--- creation_tp.php
+++ creation_tp.php
@@ -1,23 +1,47 @@
 
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset='utf-8'>
         <title>creation-TP</title>
-        <link rel="stylesheet" href="styles.css" type="text/css" />
+        <link rel='stylesheet' href='styles.css' type='text/css' />
         <?php
            session_set_cookie_params(1);
            session_start();
         ?>
     </head>
 
-    <body>
-            <div>
-                <form method="POST">
-                    <strong>De combien d'utilisateur avez vous besoin ?</strong>
-                    <input type="number" name="nombreutilisateurs" class="demande_nom_utilisateur" id="nombre_utilisateurs" min="1" max="40" value='Nombre de participants' required></input>
-                    
-                </form>
-            </div>
+    <body>  
+            <?php
+            if(isset($_POST["nombreutilisateurs"])==False){
+                echo
+                "<div class=alignement_au_centre>
+                    <form method='POST'>";
+                echo"
+                        De combien d'utilisateur avez-vous besoin ?"."
+                        <input type='number' name='nombreutilisateurs' class='demande_nom_utilisateur' id='nombre_utilisateurs' min='1' max='40' value='Nombre de participants' required></input>
+                            </br>
+                        <input type='submit' value='Valider' class='BoutonValidation' >
+                    </form>
+                </div>";
+            }
+            
+            if (isset($_POST["nombreutilisateurs"])){
+                $nombre_util= htmlspecialchars($_POST["nombreutilisateurs"]);
+                echo '
+                <div class=alignement_au_centre>
+                    <form method="POST">';
+                for($i=1;$i<=$nombre_util;$i++){
+                    echo"
+                    <input name='nom_utilisateur' value='Nom utilisateur' class='alignement_input' required>
+
+                    </br>
+                    ";
+                }
+                echo'</div>';
+            }
+
+                
+            ?>
     </div>
 
 </html>
